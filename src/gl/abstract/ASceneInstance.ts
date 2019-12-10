27ce25b161d453affc206bb97b3bb2cb737f92df
gilles boisson <gilles@dotify.fr>
2019-12-10 11:23:29 +0100

    Move type to core

--- src/gl/abstract/ASceneInstance.ts
+++ src/gl/abstract/ASceneInstance.ts
@@ -1,69 +1,60 @@
-import {mat4} from "gl-matrix";
-import {Type} from "../../helpers/Type";
-import {ITransform} from "./ITransform";
-import {MatType} from "./MatType";
-
+import { mat4 } from 'gl-matrix';
+import { Type } from '../../core/Type';
+import { ITransform } from './ITransform';
+import { MatType } from './MatType';
 
 const IDENT_MAT4 = mat4.create();
 
-
 export abstract class ASceneInstance<MatT, TransformT extends ITransform<MatT>> {
-    transform: TransformT;
-    protected _worldMat: MatT;
-    protected _nodes: ASceneInstance<MatT, ITransform<MatT>>[] = [];
-    protected _parent: ASceneInstance<MatT, ITransform<MatT>> = null;
+  transform: TransformT;
+  protected _worldMat: MatT;
+  protected _nodes: ASceneInstance<MatT, ITransform<MatT>>[] = [];
+  protected _parent: ASceneInstance<MatT, ITransform<MatT>> = null;
 
-    constructor(MatClass?: MatType<MatT>, TransformClass?: Type<TransformT>) {
-        this._worldMat = MatClass.create();
-        this.transform = new TransformClass();
+  constructor(MatClass?: MatType<MatT>, TransformClass?: Type<TransformT>) {
+    this._worldMat = MatClass.create();
+    this.transform = new TransformClass();
+  }
 
+  addChild(node: ASceneInstance<MatT, ITransform<MatT>>) {
+    if (this._nodes.indexOf(node) === -1) {
+      this._nodes.push(node);
+      node._parent = this;
     }
+  }
 
-    addChild(node: ASceneInstance<MatT, ITransform<MatT>>) {
-        if (this._nodes.indexOf(node) === -1) {
-            this._nodes.push(node);
-            node._parent = this;
-        }
+  removeChild(node: ASceneInstance<MatT, ITransform<MatT>>) {
+    const ind = this._nodes.indexOf(node);
+    if (ind !== -1) {
+      this._nodes.splice(ind, 1);
+      node._parent = null;
     }
+  }
 
-    removeChild(node: ASceneInstance<MatT, ITransform<MatT>>) {
-        const ind = this._nodes.indexOf(node);
-        if (ind !== -1) {
-            this._nodes.splice(ind, 1);
-            node._parent = null;
-        }
+  calcWorldMat(worldMat?: MatT): MatT {
+    return this.updateWorldMat(this._parent !== null ? this._parent.calcWorldMat() : null, worldMat);
+  }
+
+  getCachedWorldMat(): MatT {
+    return this._worldMat;
+  }
+
+  resolveTransformTree(parentMat?: MatT): void {
+    this.updateWorldMat(parentMat, this._worldMat);
+    for (let child of this._nodes) {
+      child.resolveTransformTree(this._worldMat);
     }
+  }
 
+  protected abstract updateWorldMat(parentMap?: MatT, worldMat?: MatT): MatT;
 
-    calcWorldMat(worldMat?: MatT): MatT {
-        return (this.updateWorldMat(
-            this._parent !== null
-                ? this._parent.calcWorldMat()
-                : null
-            , worldMat));
-    }
-
-    getCachedWorldMat(): MatT{
-        return this._worldMat;
-    }
-    
-    resolveTransformTree(parentMat?: MatT): void{
-        this.updateWorldMat(parentMat, this._worldMat);
-        for(let child of this._nodes){
-            child.resolveTransformTree(this._worldMat);
-        }
-    }
-
-    protected abstract updateWorldMat(parentMap?: MatT, worldMat?: MatT): MatT;
-    
-
-    // getWorldMat(worldMat: MatT = this._worldMat): MatT {
-    //     const localMat = this._transform.getLocalMat();
-    //
-    //     if (this._parent === null) {
-    //         return localMat;
-    //     } else {
-    //         return this.MatClass.multiply(worldMat, this._parent.getWorldMat(), localMat);
-    //     }
-    // }
+  // getWorldMat(worldMat: MatT = this._worldMat): MatT {
+  //     const localMat = this._transform.getLocalMat();
+  //
+  //     if (this._parent === null) {
+  //         return localMat;
+  //     } else {
+  //         return this.MatClass.multiply(worldMat, this._parent.getWorldMat(), localMat);
+  //     }
+  // }
 }
--- src/gl/core/data/AGLBatch.ts
+++ src/gl/core/data/AGLBatch.ts
@@ -1,130 +1,130 @@
-import {IInterleavedData, InterleaveDataT, InterleavedDataArray} from "../../data/InterleavedData";
-import {GLVao} from "./GLVao";
-import {AnyWebRenderingGLContext} from "../GLHelpers";
-import {GLBuffer} from "./GLBuffer";
-import {GLAttribute} from "./GLAttribute";
-import {Type} from "../../../helpers/Type";
+import { IInterleavedData, InterleaveDataT, InterleavedDataArray } from '../../data/InterleavedData';
+import { GLVao } from './GLVao';
+import { AnyWebRenderingGLContext } from '../GLHelpers';
+import { GLBuffer } from './GLBuffer';
+import { GLAttribute } from './GLAttribute';
+import { Type } from '../../../core/Type';
 
 export type pullMethod<DataT extends IInterleavedData> = (
-    points: DataT[],
-    pointInd: number,
-    indices?: Uint16Array,
-    indiceInd?: number,
-    indiceStride?: number,
+  points: DataT[],
+  pointInd: number,
+  indices?: Uint16Array,
+  indiceInd?: number,
+  indiceStride?: number,
 ) => void;
 
 export interface GLBatchable<DataT extends IInterleavedData> {
-    pullFromBatch: pullMethod<DataT>;
-    pointLength: number;
-    indexLength: number;
+  pullFromBatch: pullMethod<DataT>;
+  pointLength: number;
+  indexLength: number;
 }
 
-export abstract class AGLBatch<DataT extends IInterleavedData> extends InterleavedDataArray<DataT>{
-    protected _vao: GLVao;
-    protected _buffer: GLBuffer;
-    protected _indexBuffer: GLBuffer;
-    protected _indexArray: Uint16Array;
+export abstract class AGLBatch<DataT extends IInterleavedData> extends InterleavedDataArray<DataT> {
+  protected _vao: GLVao;
+  protected _buffer: GLBuffer;
+  protected _indexBuffer: GLBuffer;
+  protected _indexArray: Uint16Array;
 
-    protected _attributes: GLAttribute[];
+  protected _attributes: GLAttribute[];
 
-    protected _pointPos     = 0;
-    protected _indexPos    = 0;
-    protected _pointInc     = 0;
-    protected _indiceInc    = 0;
+  protected _pointPos = 0;
+  protected _indexPos = 0;
+  protected _pointInc = 0;
+  protected _indiceInc = 0;
 
-    constructor(
-        protected _gl: AnyWebRenderingGLContext,
-        DataClass: InterleaveDataT<DataT>,
-        pointLength: number,
-        stride: number,
-        protected _indexLength: number = -1,
-        protected _indexStride: number = -1,
-        drawType: GLenum = _gl.DYNAMIC_DRAW,
-    ){
-        super(DataClass,pointLength,stride);
-        this._buffer = new GLBuffer(_gl, _gl.ARRAY_BUFFER, drawType, this._bufferView);
+  constructor(
+    protected _gl: AnyWebRenderingGLContext,
+    DataClass: InterleaveDataT<DataT>,
+    pointLength: number,
+    stride: number,
+    protected _indexLength: number = -1,
+    protected _indexStride: number = -1,
+    drawType: GLenum = _gl.DYNAMIC_DRAW,
+  ) {
+    super(DataClass, pointLength, stride);
+    this._buffer = new GLBuffer(_gl, _gl.ARRAY_BUFFER, drawType, this._bufferView);
 
-        if(!DataClass.createAttributes){
-            throw new Error('Class must implement static method createAttributes(gl: WebGLContext, buffer: GLBuffer, stride: number) : GLAttribute[]');
-        }
-
-        if(_indexLength !== -1){
-            this._indexArray = new Uint16Array(_indexLength * _indexStride);
-            this._indexBuffer = new GLBuffer(_gl, _gl.ELEMENT_ARRAY_BUFFER, drawType, this._indexArray);
-        }
-
-        this._attributes = DataClass.createAttributes(_gl, this._buffer, this._stride) as GLAttribute[];
-        this._attributes = DataClass.createAttributes(_gl, this._buffer, this._stride) as GLAttribute[];
-        this._vao = new GLVao(_gl, this._attributes, this._indexBuffer);
+    if (!DataClass.createAttributes) {
+      throw new Error(
+        'Class must implement static method createAttributes(gl: WebGLContext, buffer: GLBuffer, stride: number) : GLAttribute[]',
+      );
     }
 
-    protected bufferData(){
-        this._buffer.bufferSubData(this._bufferView, this._stride * this._pointPos);
-        if(this._indexLength !== -1) this._indexBuffer.bufferSubData(this._indexArray, this._indexPos);
+    if (_indexLength !== -1) {
+      this._indexArray = new Uint16Array(_indexLength * _indexStride);
+      this._indexBuffer = new GLBuffer(_gl, _gl.ELEMENT_ARRAY_BUFFER, drawType, this._indexArray);
     }
 
-    public reset(){
-        this._pointPos      = 0;
-        this._indexPos     = 0;
+    this._attributes = DataClass.createAttributes(_gl, this._buffer, this._stride) as GLAttribute[];
+    this._attributes = DataClass.createAttributes(_gl, this._buffer, this._stride) as GLAttribute[];
+    this._vao = new GLVao(_gl, this._attributes, this._indexBuffer);
+  }
+
+  protected bufferData() {
+    this._buffer.bufferSubData(this._bufferView, this._stride * this._pointPos);
+    if (this._indexLength !== -1) this._indexBuffer.bufferSubData(this._indexArray, this._indexPos);
+  }
+
+  public reset() {
+    this._pointPos = 0;
+    this._indexPos = 0;
+  }
+
+  public begin() {
+    this.reset();
+  }
+
+  public push(pointLength: number, indexLength: number, pull: pullMethod<DataT>) {
+    if (this._pointPos + pointLength > this._length || this._indexPos + indexLength > this._indexLength) {
+      this.complete();
     }
 
-    public begin(){
-        this.reset();
-
+    if (this._indexLength !== -1) {
+      pull(this._collection, this._pointPos, this._indexArray, this._indexPos, this._indexStride);
+    } else {
+      pull(this._collection, this._pointPos);
     }
 
-    public push(pointLength: number, indexLength: number, pull: pullMethod<DataT>){
-        if(this._pointPos + pointLength > this._length || this._indexPos + indexLength > this._indexLength) {
-            this.complete();
-        }
+    this._pointPos += pointLength;
+    this._indexPos += indexLength;
+  }
 
-        if(this._indexLength !== -1){
-            pull(this._collection, this._pointPos, this._indexArray, this._indexPos, this._indexStride);
-
-        }else{
-            pull(this._collection, this._pointPos);
-        }
-
-        this._pointPos += pointLength;
-        this._indexPos += indexLength;
+  public pushElement(element: GLBatchable<DataT>) {
+    if (
+      this._pointPos + element.pointLength > this._length ||
+      this._indexPos + element.indexLength > this._indexLength
+    ) {
+      this.complete();
     }
 
-    public pushElement(element: GLBatchable<DataT>){
-        if(this._pointPos + element.pointLength > this._length || this._indexPos + element.indexLength > this._indexLength) {
-            this.complete();
-        }
+    element.pullFromBatch(
+      this._collection,
+      this._pointPos,
+      this._indexArray,
+      this._indexPos * this._indexStride,
+      this._indexStride,
+    );
 
-        element.pullFromBatch(
-            this._collection,
-            this._pointPos,
-            this._indexArray,
-            this._indexPos * this._indexStride,
-            this._indexStride
-        );
+    this._pointPos += element.pointLength;
+    this._indexPos += element.indexLength;
+  }
 
-        this._pointPos += element.pointLength;
-        this._indexPos += element.indexLength;
-    }
+  public complete() {
+    this.bufferData();
+    this._vao.bind();
+    this.ended(this._gl, this._pointPos, this._indexPos * this._indexStride);
+    this._vao.unbind();
+    this.reset();
+  }
 
-    public complete(){
-        this.bufferData();
-        this._vao.bind();
-        this.ended(this._gl, this._pointPos, this._indexPos * this._indexStride);
-        this._vao.unbind();
-        this.reset();
+  public end() {
+    this.complete();
+  }
 
-    }
+  abstract ended(gl: AnyWebRenderingGLContext, nbPoints: number, nbIndices: number): void;
 
-    public end(){
-        this.complete();
-    }
-
-    abstract ended(gl: AnyWebRenderingGLContext, nbPoints: number, nbIndices: number): void;
-
-    destroy(){
-        this._vao.destroy();
-        this._buffer.destroy();
-    }
-
-
+  destroy() {
+    this._vao.destroy();
+    this._buffer.destroy();
+  }
 }
