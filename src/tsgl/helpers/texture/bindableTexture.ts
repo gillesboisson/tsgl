f1178ed523db14be14f10b2eab7b89538be1aeec
Gilles Boisson <gilles@scopopop.com>
2021-03-05 21:07:37 +0100

    GLTexture to flat IGLTexture

--- src/tsgl/helpers/texture/bindableTexture.ts
+++ src/tsgl/helpers/texture/bindableTexture.ts
@@ -1,31 +1,25 @@
 import { AnyWebRenderingGLContext } from '../../gl/core/GLHelpers';
 import { IGLTexture, IGLTextureBase } from '../../gl/core/GLTexture';
 
-
-export function wrapTexture(gl: AnyWebRenderingGLContext, texture: WebGLTexture, target: GLenum): IGLTexture{
-
+export function wrapTexture(gl: AnyWebRenderingGLContext, texture: WebGLTexture, target: GLenum): IGLTexture {
   return bindableTexture({
     gl,
     texture,
     target,
-  })
-
+  });
 }
 
 export function bindableTexture<T extends IGLTextureBase = IGLTextureBase>(
   // gl: AnyWebRenderingGLContext,
   sourceTexture: T,
   // type: GLenum,
-
 ): T & {
   bind: () => void;
   unbind: () => void;
   active: (index?: number) => void;
   safeBind: (bindExec: (texture: IGLTexture) => void) => void;
 } {
-
-
-  const {gl, texture, target} = sourceTexture;
+  const { gl, texture, target } = sourceTexture;
   const res = {
     ...sourceTexture,
     bind,
@@ -46,16 +40,12 @@ export function bindableTexture<T extends IGLTextureBase = IGLTextureBase>(
     gl.activeTexture(gl.TEXTURE0 + index);
     gl.bindTexture(target, texture);
   }
-  
-  
 
-  function safeBind(bindExec: (texture: IGLTexture) => void){
+  function safeBind(bindExec: (texture: IGLTexture) => void) {
     gl.bindTexture(target, texture);
     bindExec(res as any);
     gl.bindTexture(target, null);
   }
 
-
   return res as any;
- 
 }
