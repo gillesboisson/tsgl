124830677cd32e1deab64b5f27082c9862eb2d4a
Gilles Boisson <gilles@scopopop.com>
2021-06-12 20:17:03 +0200

    Simpler implementation example

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { mat4, vec3, vec4 } from 'gl-matrix';
+import { vec3 } from 'gl-matrix';
 
 import { Base3DApp } from './app/Base3DApp';
-import { createFBAndFlippableTexture } from './tsgl/helpers/texture/createFBAndFlippableTexture';
-import { createEmptyMipmapTexture } from './tsgl/helpers/texture/createEmptyMipmapTexture';
-import { createMipmapTextureForStorage } from './tsgl/helpers/texture/createMipmapTextureForStorage';
-import { createCubemapEmptyTexture } from './tsgl/helpers/texture/createCubemapEmptyTexture';
-import { TestFlatMaterial } from './app/materials/TestFlatMaterial';
-import { CopyShader, CopyShaderID, CopyShaderState } from './app/shaders/CopyShader';
-import { TestBlurShader, TestBlurShaderID, TestBlurShaderState } from './app/shaders/TestBlurShader';
-import { TestFlatShader } from './app/shaders/TestFlatShader';
-import { TestLodShader, TestLodShaderID, TestLodShaderState } from './app/shaders/TestLodShader';
-import { LambertVShader } from './app/shaders/VariantShaderTest';
-import { Camera } from './tsgl/3d/Camera';
-import { GLTFData } from './tsgl/3d/gltf/GLFTSchema';
-import { GLTFNode } from './tsgl/3d/gltf/GLTFNode';
-import {
-  setBufferViewTargetFromMesh,
-  loadBuffers,
-  getBufferViewsDataLinkedToBuffer,
-  loadBufferView,
-  createMesh,
-  loadTextures,
-} from './tsgl/3d/gltf/GLTFParser';
 import { IRenderableInstance3D } from './tsgl/3d/IRenderableInstance3D';
-import { DepthOnlyMaterial } from './tsgl/3d/Material/DepthOnlyMaterial';
 import { PhongBlinnMaterial } from './tsgl/3d/Material/PhongBlinnMaterial';
-import { ShadowOnlyMaterial } from './tsgl/3d/Material/ShadownOnlyMaterial';
-import { SkyboxMaterial } from './tsgl/3d/Material/SkyboxMaterial';
 import { MeshNode, SceneInstance3D } from './tsgl/3d/SceneInstance3D';
 import { ShadowMap } from './tsgl/3d/ShadowMap';
-import { CameraLookAtTransform3D } from './tsgl/geom/CameraTargetTransform3D';
-import { CubeMapPatronHelper } from './tsgl/geom/CubeMapPatronHelper';
-import { createBoxMesh, cubeSquarePatronUv } from './tsgl/geom/mesh/createBoxMesh';
-import { createCylinderMesh } from './tsgl/geom/mesh/createCylinderMesh';
 import { createPlaneMesh } from './tsgl/geom/mesh/createPlaneMesh';
-import { createQuadMesh } from './tsgl/geom/mesh/createQuadMesh';
-import { createSkyBoxMesh } from './tsgl/geom/mesh/createSkyBoxMesh';
 import { createSphereMesh } from './tsgl/geom/mesh/createSphereMesh';
 import { Transform3D } from './tsgl/geom/Transform3D';
-import { GLBuffer } from './tsgl/gl/core/data/GLBuffer';
-import { GLMesh } from './tsgl/gl/core/data/GLMesh';
 import { GLVao } from './tsgl/gl/core/data/GLVao';
 import { GLFramebuffer } from './tsgl/gl/core/framebuffer/GLFramebuffer';
 import { GLViewportStack } from './tsgl/gl/core/framebuffer/GLViewportState';
-import { GLRenderer, GLRendererType, WebGL2Renderer } from './tsgl/gl/core/GLRenderer';
-import { IGLTexture } from './tsgl/gl/core/texture/GLTexture';
+import { GLRendererType, WebGL2Renderer } from './tsgl/gl/core/GLRenderer';
 import { FirstPersonCameraController } from './tsgl/input/CameraController';
-import { BasicColorShader } from './tsgl/shaders/BasicColorShader';
-import { BasicTextureShaderState, BasicTextureShader, BasicTextureShaderID } from './tsgl/shaders/BasicTextureShader';
 import { DepthOnlyShader } from './tsgl/shaders/DepthOnlyShader';
-import { MSDFShader } from './tsgl/shaders/MSDFShader';
 import { PhongBlinnVShader } from './tsgl/shaders/PhongBlinnVShader';
-import {
-  PlaneSpaceToModelSpaceNormalShaderState,
-  PlaneSpaceToModelSpaceNormalShader,
-} from './tsgl/shaders/PlaceSpaceToModelSpaceNormalShader';
-import { ShadowOnlyShader } from './tsgl/shaders/ShadowOnlyShader';
-import { SimpleLamberianShader } from './tsgl/shaders/SimpleLamberianShader';
-import { SimplePBRShader } from './tsgl/shaders/SimplePBRShader';
-import { SimpleTextureShader } from './tsgl/shaders/SimpleTextureShader';
-import { SkyboxShader } from './tsgl/shaders/SkyboxShader';
 
-import { floatToRgb9_e5, loadHDR, loadHDRToFloatTexture, rgbeToFloat } from './tsgl/helpers/texture/hdr';
-import { HDRToCubemap } from './tsgl/baking/HDRRectToCubemap';
-import { createFramebufferWithDepthStorage } from './tsgl/helpers/framebuffer';
-import { renderFaces } from './tsgl/helpers/texture/CubemapRenderer';
-import { createCubemapMipmapEmptyTexture } from './tsgl/helpers/texture/createCubemapMipmapEmptyTexture';
-import {
-  DebugSkyboxLodShader,
-  DebugSkyboxLodShaderID,
-  DebugSkyboxLodShaderState,
-} from './app/shaders/DebugSkyboxLodShader';
-import { BrdfLutShader, BrdfLutShaderID, BrdfLutShaderState } from './tsgl/shaders/BrdfLutShader';
-import { createEmptyTextureWithLinearFilter } from './tsgl/helpers/texture/createEmptyTextureWithLinearFilter';
-import { Quad } from './tsgl/2d/sprite/Quad';
-import { EquiToCubemapShader } from './tsgl/shaders/EquiToCubemapShader';
-import { IrradianceShader } from './tsgl/shaders/IrradianceShader';
-import { ReflectanceShader, ReflectanceShaderState, ReflectanceShaderID } from './tsgl/shaders/ReflectanceShader';
-import { ReflectanceCubemapRenderer } from './tsgl/baking/ReflectanceCubemapRenderer';
-import { IrradianceCubemapRenderer } from './tsgl/baking/IrradianceCubemapRenderer';
-import { bakeHdrIbl } from './tsgl/baking/bakeHdrIbl';
-import { PbrShaderDebug, PbrVShader } from './tsgl/shaders/PbrVShader';
-import { PbrMaterial } from './tsgl/3d/Material/PbrMaterial';
+import { PbrVShader } from './tsgl/shaders/PbrVShader';
 import { loadTexture2D } from './tsgl/helpers/texture/loadTexture2D';
-import { GLTFMaterialFactory } from './tsgl/3d/gltf/GLTFMaterialFactory';
-import { VertexColorShader, VertexColorShaderState } from './tsgl/shaders/VertexColorShader';
+import { VertexColorShaderState } from './tsgl/shaders/VertexColorShader';
 import {
-  IWireframeBatchPullable,
-  VertexColorData,
   WireframeBatch,
-  WireframeBatchRenderable,
 } from './tsgl/3d/helpers/WireframeBatch';
-import { SimpleTextureMaterial } from './tsgl/3d/Material/SimpleTextureMaterial';
-// import { PbrMaterial } from './tsgl/3d/Material/PbrMaterial';
 
 window.addEventListener('load', async () => {
   const app = new TestApp();
